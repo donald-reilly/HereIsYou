13f5167b33b2c4bf357e9480c6f524a491798b48
Dub <donald.reilly.jr@outlook.com>
2026-07-12 09:32:10 -0700

    Rename: HereIsJR has been renamed to You

---
+++ src/HereIsYou/You/__init__.py
@@ -0,0 +1,9 @@
+from .introspection import You
+"""
+BInspected: A recursive Python object introspection engine.
+This module provides the BInspected class, which classifies Python
+objects, extracts metadata, groups children, and recursively builds
+a structured introspection dictionary.
+"""
+
+__all__ = ['You']
